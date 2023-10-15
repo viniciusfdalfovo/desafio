7ea69e3122078abf9e15b4230fa7773b89a7ca73
viniciusfdalfovo <147889699+viniciusfdalfovo@users.noreply.github.com>
2023-10-15 17:09:07 -0300

    Update index.php

--- apiv2/index.php
+++ apiv2/index.php
@@ -1,7 +1,6 @@
 <?php
 // Configurar os parâmetros do cURL
 
-
 // Inicializar uma sessão cURL
 $ch = curl_init();
 // Definir URL do endpoint
@@ -14,10 +13,7 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_TIMEOUT, 20);
 
 
-
-
 // Armazenar e consultar os pré-resultados da cURL
-
 
 // Guardar o valor 'marca' consultado na variável 'input'
 $input = $_GET['marca'];
@@ -31,10 +27,7 @@ $brands = array_column($decoded, 'nome', 'codigo');
 $brand_found = array_search("$input", $brands);
 
 
-
-
 // Validar as informações inseridas e retornadas e exibir o resultado final ou erros
-
 
 // Validar se houve erros na comunicação com a API
 if(curl_error($ch)){
@@ -57,21 +50,24 @@ else {
     // Decodificar 'resp2' para o array 'decoded2'
     $decoded2 = json_decode($fresp, true);
     // Selecionar as entradas 'modelos' no array relacional 'decode2' e guardar em 'models'
-    $models = $decoded2[modelos];
+    $models = $decoded2['modelos'];
     //Contar o número de entradas no array models e armazenar em 'nmod'
     $nmod = count($models);
 
     // Iteração de 0 até o número de modelos
     for ($i = 0;$i < $nmod; $i++){
         // Armazenar em 'modesp' o modelo específico, de acordo com sua posição no array
-        $modesp = $models[$i][nome];
+        $modesp = $models[$i]['nome'];
         // Armazenar em 'codesp' o código específico do modelo, de acordo com sua posição no array
-        $codesp = $models[$i][codigo];
+        $codesp = $models[$i]['codigo'];
+        // Criar um novo array 'aoutput' coms os dados especificos
         $aoutput = [
-            ['vehicle'=>"$input",'code'=>"$brand_found"],
-            ['vehicle'=>"$modesp",'code'=>"$codesp"]
+            ['vehicle'=>$input,'code'=>$brand_found],
+            ['vehicle'=>$modesp,'code'=>$codesp]
         ];
+        // Formatar em JSON, caso necessario
         $joutput = json_encode($aoutput);
+        // Faz o output da API no formato de array
         var_dump($aoutput);
     }
 }
